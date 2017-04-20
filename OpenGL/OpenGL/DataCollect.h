33a85f07eaf61459a40a998df2a1dfe658ef11b2
ddk <ddkv587@gmail.com>
2017-04-20 09:10:59 +0800

    add data

--- OpenGL/OpenGL/DataCollect.h
+++ OpenGL/OpenGL/DataCollect.h
@@ -12,7 +12,7 @@
 #include <GL/glew.h>
 #include <glm/glm.hpp>
 
-GLfloat vertices[] = {
+GLfloat vertices[] = { 
 	//     ---- 位置 ----       ---- 颜色 ----     - 纹理坐标 -
 	 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   0.0f, 1.0f,   // 右上
 	 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   0.0f, 0.0f,   // 右下
